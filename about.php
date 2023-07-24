9ef856646f56b5b31addc6fad2122b59393a4a12
Elkana Sampath <90978061+ElkHub@users.noreply.github.com>
2023-07-24 21:18:00 +0530

    second commit

--- about.php
+++ about.php
@@ -5,18 +5,11 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>home</title>
-
-   <!-- swiper css link  -->
-   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
+   
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
 
    <style>
-      /* Add custom styles for the about section here */
       .about-section {
          padding: 50px;
          background-color: #f8f8f8;
@@ -36,8 +29,6 @@
    </style>
 </head>
 <body>
-   
-<!-- header section starts  -->
 
 <section class="header">
 
@@ -50,15 +41,10 @@
    </nav>
 
 </section>
-
-<!-- About section starts here -->
+   
 <section class="about-section">
    <h1 class="about-heading">Welcome to Grace Medical Center</h1>
    <p class="about-paragraph">Your trusted healthcare partner. We are committed to providing exceptional medical care and personalized attention to all our patients. With a team of experienced and compassionate healthcare professionals, we strive to ensure your well-being and recovery. Our state-of-the-art facilities and advanced medical technologies enable us to deliver the highest standard of healthcare services. At Grace Medical Center, your health and comfort are our top priorities, and we are dedicated to serving you with utmost care and professionalism.</p>
 </section>
-<!-- About section ends here -->
-
-<!-- Rest of your HTML content goes here -->
-
 </body>
 </html>
